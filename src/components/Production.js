dcc464acb6c35681f2ecde5581a95a35640db7de
JingruZhang <zhang.jingru.93@gmail.com>
2018-04-26 16:01:01 -0700

    seperate presentational from container components

--- src/components/Production.js
+++ src/components/Production.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
-class Production extends Component {
-
-  render() {
-    return (
-      <li>{this.props.name}</li>
-    );
-  }
-}
+const Production = (props) => {
+  return(<li>{props.name}</li>);
+};
 
 export default Production;
