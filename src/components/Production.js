60cb5406ddeca1fc0c3727b76f63d82456e3995a
JingruZhang <zhang.jingru.93@gmail.com>
2018-04-26 15:51:58 -0700

    set up cors ajax call to api

---
+++ src/components/Production.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Production extends Component {
+
+  render() {
+    return (
+      <li>{this.props.name}</li>
+    );
+  }
+}
+
+export default Production;
---
+++ src/components/ProductionsContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Production from './Production';
+
+class ProductionsContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {productions: []};
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/productions')
+    .then(resp => this.setState({productions: resp.data}))
+    .catch(err => console.log(err));
+  };
+
+  render() {
+    const productionsList = this.state.productions.map((prod, index) => {
+      return(<Production key={index} name={prod.name} />)
+    });
+
+    return (
+      <div>{productionsList}</div>
+    );
+  };
+};
+
+export default ProductionsContainer;
